86c9961d1b3677130bf4444290dfcb714e73218b
yuhei.nagano <yuheinagano8942@gmail.com>
2019-01-05 10:16:30 +0000

    first commit6

--- js/main.js
+++ js/main.js
@@ -2,25 +2,37 @@
   'use strict';
   
   var ths = document.getElementsByTagName('th');
-  var i;
+  var sortOrder = 1;
+  
+  function rebuildTbody(rows) {
+     var tbody = document.querySelector('tbody');
+     var i;
+      while (tbody.firstChild) {
+        tbody.removeChild(tbody.firstChild);
+      }
+      for (i = 0; i < rows.length; i++) {
+        tbody.appendChild(rows[i]);
+    }
+  }
   
   
-  for (i = 0; i < ths.length; i++) {
-    ths[i].addEventListener('click', function() {
-     
+  function updateClassName(th) {
+      var k;
+      for (k = 0; k < ths.length; k++) {
+        ths[k].className = '';
+      }
+      th.className = sortOrder === 1 ? 'asc' : 'desc';
+    }
     
-      var rows = Array.prototype.slice.call(document.querySelectorAll('tbody > tr'));
-      var col = this.cellIndex;
-      var type = this.dataset.type;
-      
-      rows.sort(function(a, b) {
+    function compare(a, b, col, type) {
+        var _a = a.children[col].textContent;
+        var _b = b.children[col].textContent;
         if (type === "number") {
-          var _a = a.children[col].textContent * 1;
-          var _b = b.children[col].textContent * 1;
-        }
-        if (type === "string") {
-          var _a = a.children[col].textContent.toLowerCase();
-          var _b = b.children[col].textContent.toLowerCase();
+          _a = _a * 1;
+          _b = _b * 1;
+        } else if (type === "string") {
+          _a = _a.toLowerCase();
+          _b = _b.toLowerCase();
         }
         if (_a < _b) {
           return -1;
@@ -29,19 +41,32 @@
           return 1;
         }
         return 0;
+      }
+      
+      function sortRows(th) {
+      var rows = Array.prototype.slice.call(document.querySelectorAll('tbody > tr'));
+      var col = th.cellIndex;
+      var type = th.dataset.type;
+      
+      rows.sort(function(a, b) {
+        return compare(a, b, col, type) * sortOrder;
       });
-      
-      var tbody = document.querySelector('tbody');
-      
-      while (tbody.firstChild) {
-        tbody.removeChild(tbody.firstChild);
-      }
-      
-      var j;
-      for (j = 0; j < rows.length; j++) {
-        tbody.appendChild(rows[j]);
-      }
-    });
-  }
+      return rows;
+    }
   
+  
+  function setup() {
+    var i;
+    for (i = 0; i < ths.length; i++) {
+    ths[i].addEventListener('click', function() {
+      var rows;
+      rows = sortRows(this);
+      rebuildTbody(rows);
+      updateClassName(this);
+      sortOrder *= -1;
+    });
+   }
+  }  
+  
+  setup();
 })();
