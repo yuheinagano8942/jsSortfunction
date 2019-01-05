e426672a7b8c5633170fcbeae7b3a992614438ae
yuhei.nagano <yuheinagano8942@gmail.com>
2019-01-05 09:08:06 +0000

    first commit5

--- js/main.js
+++ js/main.js
@@ -7,14 +7,21 @@
   
   for (i = 0; i < ths.length; i++) {
     ths[i].addEventListener('click', function() {
-      //sort
-     // var arr = ['taguchi', 'fkoji', 'dotinstall'];
+     
+    
       var rows = Array.prototype.slice.call(document.querySelectorAll('tbody > tr'));
       var col = this.cellIndex;
+      var type = this.dataset.type;
       
       rows.sort(function(a, b) {
-        var _a = a.children[col].textContent;
-        var _b = b.children[col].textContent;
+        if (type === "number") {
+          var _a = a.children[col].textContent * 1;
+          var _b = b.children[col].textContent * 1;
+        }
+        if (type === "string") {
+          var _a = a.children[col].textContent.toLowerCase();
+          var _b = b.children[col].textContent.toLowerCase();
+        }
         if (_a < _b) {
           return -1;
         }
@@ -23,6 +30,17 @@
         }
         return 0;
       });
+      
+      var tbody = document.querySelector('tbody');
+      
+      while (tbody.firstChild) {
+        tbody.removeChild(tbody.firstChild);
+      }
+      
+      var j;
+      for (j = 0; j < rows.length; j++) {
+        tbody.appendChild(rows[j]);
+      }
     });
   }
   
